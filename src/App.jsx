d358977567af6f1c5e3abb6cc97c1475e86f6ec5
zainkhan <khanzain33999@gmail.com>
2024-08-20 00:33:24 +0500

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
-        import './App.css'
-        import Footer from './components/Footer'
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Recipes from './components/Recipes';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Home from './components/Home';
 
-        import Navbar from './components/Navbar'
-        import { BrowserRouter, Routes, Route } from "react-router-dom";
-        import Recipes from './components/Recipes';
-        import {  useEffect, useState } from "react";
-        import axios from 'axios';
-    import Home from './components/Home';
-        function App() {
-          const [displayQuery, setDisplayQuery] = useState("Search recipe name...");
-          const [recipes, setRecipes] = useState([])
-          const [searchQuery, setSearchQuery] = useState("");
-        
-          
-    const [favArray, setfavArray] = useState([])
+function App() {
+  const [displayQuery, setDisplayQuery] = useState("Search recipe name...");
+  const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [favArray, setfavArray] = useState([]);
 
-        const searchRecipes = async () => {
+  const searchRecipes = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
+      );
+      const fetchedRecipes = response.data.meals || [];
 
+      setDisplayQuery(searchQuery);
+      setRecipes(fetchedRecipes);
+      
+      // Save search results to local storage
+      sessionStorage.setItem('searchedRecipes', JSON.stringify({ query: searchQuery, recipes: fetchedRecipes }));
+      
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-            try {
-            const response = await axios.get(
-                `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
-            
-            );
-            //   console.log(response.data.meals)
-            setDisplayQuery(searchQuery);   
-            setRecipes(response.data.meals || []); 
-            } catch (err) {
-            console.log(err);
-            } 
-        };
-        const favRecipes = (strMeal, strMealThumb, strCategory, strArea) => {
-          const newRecipe = { name: strMeal, img: strMealThumb, cat: strCategory, area: strArea };
-        
-          if (!favArray.some(recipe => recipe.name === strMeal)) {
-            const updatedFavArray = [...favArray, newRecipe];
-            setfavArray(updatedFavArray);
-            localStorage.setItem('favoriteRecipes', JSON.stringify(updatedFavArray)); 
-          }
-        };
-        
-        useEffect(() => {
-          const storedFavRecipes = localStorage.getItem('favoriteRecipes');
-          if (storedFavRecipes) {
-            setfavArray(JSON.parse(storedFavRecipes));
-          }
-        }, []); 
+  const favRecipes = (strMeal, strMealThumb, strCategory, strArea) => {
+    const newRecipe = { name: strMeal, img: strMealThumb, cat: strCategory, area: strArea };
+    
+    if (!favArray.some(recipe => recipe.name === strMeal)) {
+      const updatedFavArray = [...favArray, newRecipe];
+      setfavArray(updatedFavArray);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(updatedFavArray)); 
+    }
+  };
+
+  useEffect(() => {
+
+    const storedFavRecipes = localStorage.getItem('favoriteRecipes');
+    if (storedFavRecipes) {
+      setfavArray(JSON.parse(storedFavRecipes));
+    }
+
+    const storedSearchData = sessionStorage.getItem('searchedRecipes');
+    if (storedSearchData) {
+      const { query, recipes } = JSON.parse(storedSearchData);
+      setSearchQuery(query);
+      setDisplayQuery(query);
+      setRecipes(recipes);
+    }
+  }, []);
           return (
             <div className="flex flex-col min-h-screen overflow-hidden">
             <BrowserRouter>
